75faee4375f0088ac2e3bf3cb3c9aa1e8ce640de
Jesus <jesus.jimenez@upr.edu>
2023-06-19 22:02:02 -0400

    addd Data in CSV, Json, SQL Schema and notebook

---
+++ Stock_Schema.sql
@@ -0,0 +1,123 @@
+DROP TABLE IF EXISTS TSLA;
+DROP TABLE IF EXISTS SQM;
+DROP TABLE IF EXISTS SGML;
+DROP TABLE IF EXISTS Samsung;
+DROP TABLE IF EXISTS PILBF;
+DROP TABLE IF EXISTS NIO;
+DROP TABLE IF EXISTS MALRY;
+DROP TABLE IF EXISTS LTHM;
+DROP TABLE IF EXISTS LAC;
+DROP TABLE IF EXISTS GNENF;
+DROP TABLE IF EXISTS ALB;
+
+Create Table TSLA (
+	date date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table SQM (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+	
+Create Table SGML (
+	"date" date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table Samsung (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table PILBF (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+
+Create Table NIO (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table MALRY (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);	
+	
+Create Table LTHM (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table LAC (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table GNENF (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+	
+Create Table ALB (
+	"date"date NOT NULL,
+	"Open" Float, 
+	"High" Float, 
+	"Low" FLoat,
+	"Close" Float, 
+	"Adj Close" Float,
+	"volume" Float);
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+Select * from "Samsung";
