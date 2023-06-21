e895b5b08514ce14226cbc9d33517f8daeabb78b
SH-Mars <100989742+SH-Mars@users.noreply.github.com>
2023-06-20 20:14:51 -0700

    Added html css and updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-# Import the dependencies.
-import numpy as np
-from pathlib import Path
-import json
+import datetime
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
-from flask import Flask
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine(f"postgresql+psycopg2://username:password@localhost:5432/Stocks")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -29,74 +43,206 @@ def home():
         "<a href='http://127.0.0.1:5000/api/v1.0/SGML'>SGML</a><br/>"
         "<a href='http://127.0.0.1:5000/api/v1.0/SQM'>SQM</a><br/>"
         "<a href='http://127.0.0.1:5000/api/v1.0/TSLA'>TSLA</a><br/>"
-
     )
 
 @app.route('/api/v1.0/ALB')
 def ALB():
-    with open ("Stock/alb_Stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM alb")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/GNENF')
 def GNENF():
-    with open ("Stock/gnenf_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM gnenf")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/LAC')
 def LAC():
-    with open ("Stock/lac_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM lac")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
+    
 
 @app.route('/api/v1.0/LTHM')
 def LTHM():
-    with open ("Stock/lthm_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM lthm")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/MALRY')
 def MALRY():
-    with open ("Stock/malry_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM malry")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/NIO')
 def NIO():
-    with open ("Stock/nio_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM nio")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/PILBF')
 def PILBF():
-    with open ("Stock/pilbf_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM pilbf")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/Samsung')
 def Samsung():
-    with open ("Stock/samsung_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM samsung")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/SGML')
 def SGML():
-    with open ("Stock/sgml_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM sgml")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/SQM')
 def SQM():
-    with open ("Stock/sqm_stock.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM sqm")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 @app.route('/api/v1.0/TSLA')
 def TSLA():
-    with open ("Stock/TSLA_Stocks.json", "r") as json_file:
-        data = json.load(json_file)
-    return data
+    session = Session(engine)
+    stock = []
+    stock_data = engine.execute("SELECT * FROM tsla")
+    for results in stock_data:
+        stock_dict = {}
+        stock_dict["date"] = results[0]
+        stock_dict["open"] = results[1]
+        stock_dict["high"] = results[2]
+        stock_dict["low"] = results[3]
+        stock_dict["close"] = results[4]
+        stock_dict["adj_close"] = results[5]
+        stock_dict["volume"] = results[6]
+        stock.append(stock_dict)
+    session.close()
+    return jsonify(stock)
 
 if __name__ == '__main__':
     app.run(debug=True)
